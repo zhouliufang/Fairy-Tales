c81781ec66aea7650d3348a9c45ef45abdf74ae1
zhouliufang <zhouliufang@lihe.com>
2019-03-23 11:55:02 +0800

    初始页面

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./assets/css/common.scss";
+#app {
+  width: 100%;
+  height:100%;
+  text-align: center;
+  color: #553f2a;
+  background: #72d2eb;
+  padding: 20px;
+}
+</style>
---
+++ src/components/MainHello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pink_back">
+    <h1>Welcome to Fairy Kingdom</h1>
+    <div class="content">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainHello',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pink_back{
+  width: 100%;
+  height:100%;
+  .content{
+    width: 600px;
+    height: 400px;
+  }
+}
+</style>
